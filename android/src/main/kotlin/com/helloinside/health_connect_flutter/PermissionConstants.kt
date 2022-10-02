e64c144d04fd986b748bae0efb814c9d880d472a
dkoscica <>
2022-10-02 17:16:45 +0200

    Update permission constants

--- android/src/main/kotlin/com/helloinside/health_connect_flutter/PermissionConstants.kt
+++ android/src/main/kotlin/com/helloinside/health_connect_flutter/PermissionConstants.kt
@@ -7,7 +7,7 @@ const val TAG = "health_connect_plugin"
 class Permission {
 
     object Type {
-        const val ACTIVITY_RECOGNITION_PERMISSION = "com.google.android.gms.permission.ACTIVITY_RECOGNITION"
+        const val ACTIVITY_RECOGNITION_PERMISSION = "Manifest.permission.ACTIVITY_RECOGNITION"
         const val ACCESS_FINE_LOCATION = Manifest.permission.ACCESS_FINE_LOCATION
         const val BODY_SENSORS = Manifest.permission.BODY_SENSORS
     }
